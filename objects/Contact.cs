f5e044ccfe7bb8138f624e19c3d74efc1f85bc9c
russ <russvetsper@gmail.com>
2016-07-15 13:13:07 -0700

    add public class and private string to contact.cs

---
+++ objects/Contact.cs
@@ -0,0 +1,18 @@
+using System.Collection.Generic;
+
+namespace Addressbook.objects
+{
+  public class contact
+  {
+    private string _fullName;
+    private string _phoneNumber;
+    private string _address;
+    private string _city;
+    private string _state;
+
+    private int _id;
+
+  
+
+  }
+}
