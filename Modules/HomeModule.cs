5ecb0f7152e3265226c7c5895c4c177674a7867f
russ <russvetsper@gmail.com>
2016-07-15 14:54:59 -0700

    add find paremeters

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -23,11 +23,16 @@ namespace Addressbook
         return View["new_contact.cshtml"];
       };
 
+      Get["/view_all_contact"] = _ = {
+        List<Contact> allContacts = Contact.GetAll();
+        return View["view_all_contact.cshtml", allContacts];
+      };
 
+      get["/contact/{id}"] = parameters => {
+        Contact oneContact = Contact.Find(parameters.id);
+        return View["contact.cshtml",oneContact];
+      };
 
-
-
-      
 
       Post["/new_contact_add"] = _ => {
        Contact newContact = new Contact
@@ -35,10 +40,9 @@ namespace Addressbook
          Request.Form["contact-phoneNumber"],
          Request.Form["contact-address1"],
          Request.Form["contact-city"],
-         Request.Form["contact-state"];
-       return View["new_contact_add.cshtml", newContact];
+         Request.Form["contact-state"]);
+        return View["new_contact_add.cshtml", newContact];
       };
-
     }
   }
 }
