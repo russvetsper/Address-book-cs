590ffa4d89ecc15c7fa51521abaab99a0e2effa0
russ <russvetsper@gmail.com>
2016-07-15 14:08:35 -0700

    add public string and public void

--- objects/Contact.cs
+++ objects/Contact.cs
@@ -12,7 +12,69 @@ namespace Addressbook.objects
 
     private int _id;
 
-  
+    private contact(string fullName, string phoneNumber, string address, string city, string state )
+    {
+      _fullName = fullName;
+      _phoneNumber = phoneNumber;
+      _address = address;
+      _city = city
+      _state = state
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
 
+    public string GetFullName()
+    {
+      return _fullName;
+    }
+    public void SetFullName(string newFullName)
+    {
+      _fullName =newFullName;
+    }
+
+
+    public string getPhoneNumber()
+    {
+      return PhoneNumber
+    }
+      public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+
+
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+
+
+      public string GetCity()
+      {
+        return _city;
+      }
+      public void SetCity(string newCity)
+      {
+        _city = newCity;
+      }
+
+
+      public string GetState()
+      {
+        return _state;
+      }
+      public void SetState(string newState)
+      {
+        _state = newState;
+      }
+
+      public static List<Contact> GetAll()
+      {
+        return _instances
+      }
   }
 }
